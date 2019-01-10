7003a30790e429f32a6f8cfbf913368aa75059ac
ZhengHaiQiang <885479297@qq.com>
2019-01-10 17:43:46 +0800

    add dragCell 自定义滑动删除操作组件

---
+++ vue-more-simple/src/pages/viewer2.vue
@@ -0,0 +1,53 @@
+<template>
+	<section>
+		<div style="border: 1px solid red;">
+			<template v-for="(item1, index1) in imgList">
+				<viewer :images="imgList">
+					<img v-if="index1 == 0" v-for="(item2, index2) in imgList" class="img" :src="item2.url" :key="item2.url" alt="" />
+				</viewer>
+			</template>
+		</div>
+	</section>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				imgList:[{
+					url: "https://smartecloud.oss-cn-hangzhou.aliyuncs.com/b2e3a6d9-85fc-4893-ba1c-b6c1b502cdc7",
+					name:'图片1'
+				},{
+					url: "https://smartecloud.oss-cn-hangzhou.aliyuncs.com/0befa942-6a9d-4808-a855-6022c231b768",
+					name:'图片2'
+				}],
+				platform:1
+			}
+		},
+		created() {
+			window.document.documentElement.setAttribute("data-platform", "app");
+			switch(this.platform){
+				case '1':
+					window.document.documentElement.setAttribute("data-platform", "app");
+					break;
+				case '9':
+					window.document.documentElement.setAttribute("data-platform", "web");
+					break;
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style scoped="scoped">
+	.img{
+		width: 0.44rem;
+		height: 0.44rem;
+		/*[data-platform="app"] & {*/
+	        border: 1px solid red;
+	        margin-right: 0.1rem;
+	    /*}*/
+	}
+</style>
--- vue-more-simple/src/router/index.js
+++ vue-more-simple/src/router/index.js
@@ -34,6 +34,14 @@ export default new Router({
 			path:'/scrollTest',
 			name: 'scrollTest',
 			component: ()=> import('@/pages/scrollTest')
+		},{
+			path:'/viewer2',
+			name: 'viewer2',
+			component: ()=> import('@/pages/viewer2')
+		},{
+			path:'/dragCell',
+			name: 'dragCell',
+			component: ()=> import('@/pages/dragCell/dragCell')
 		}]
 	}]
 })
